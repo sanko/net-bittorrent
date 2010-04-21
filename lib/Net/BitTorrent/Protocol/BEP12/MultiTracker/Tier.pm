e1be93d970279365afe49ba3a8028ec31823b624
Sanko Robinson <sanko@cpan.org>
2010-04-22 06:24:44 +0800

    BEP12::MultiTracker::Tier is now a subclass of BEP03::Tracker (...is that what I really want?)

--- lib/Net/BitTorrent/Protocol/BEP12/MultiTracker/Tier.pm
+++ lib/Net/BitTorrent/Protocol/BEP12/MultiTracker/Tier.pm
@@ -6,8 +6,7 @@ package Net::BitTorrent::Protocol::BEP12::MultiTracker::Tier;
     use List::Util qw[shuffle];
     our $MAJOR = 0.075; our $MINOR = 0; our $DEV = 1; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
     use lib '../../../../../';
-
-    #extends 'Net::BitTorrent::Protocol::BEP03::Tracker';
+    extends 'Net::BitTorrent::Protocol::BEP03::Tracker';
     use Net::BitTorrent::Types qw[:tracker];
     use Net::BitTorrent::Protocol::BEP03::Tracker::HTTP;
     use Net::BitTorrent::Protocol::BEP15::Tracker::UDP;
