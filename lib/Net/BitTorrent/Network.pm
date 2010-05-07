c8b1958c85f231b899f21efc82d39e3d615cbd87
Sanko Robinson <sanko@cpan.org>
2010-05-07 10:41:19 +0800

    Callbacks should never be private

--- lib/Net/BitTorrent/Network.pm
+++ lib/Net/BitTorrent/Network.pm
@@ -60,8 +60,8 @@ package Net::BitTorrent::Network;
         }
         if (defined $args->{'on_data_in'})
         {    # XXX - Separate callbacks are undocumented
-            $self->_ipv4_on_data_in($args->{'on_data_in'});
-            $self->_ipv6_on_data_in($args->{'on_data_in'});
+            $self->ipv4_on_data_in($args->{'on_data_in'});
+            $self->ipv6_on_data_in($args->{'on_data_in'});
         }
         {    # Non-blocking socket creation
             my @sock_types = grep {
