f97b0ddbff9f848a9e0cdb47d803f01c693bb3f2
Sanko Robinson <sanko@cpan.org>
2010-05-18 06:28:13 +0800

    Clean up ::DHT::Standalone role

--- lib/Net/BitTorrent/DHT/Standalone.pm
+++ lib/Net/BitTorrent/DHT/Standalone.pm
@@ -1,13 +1,14 @@
 package Net::BitTorrent::DHT::Standalone;
 {
     use Moose::Role;
-    use lib '../../../../lib';
     use Net::BitTorrent::Network::UDP;
     our $MAJOR = 0.075; our $MINOR = 0; our $DEV = -1; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
-
-    #If I weren't a role, I'd...
-    #extends 'Net::BitTorrent::DHT';
-    #has '+client' => (required => 0, handles => {});
+    has 'port' => (is      => 'ro',
+                   lazy    => 1,
+                   default => 0,
+                   isa     => 'Int',
+                   writer  => '_port'
+    );
     has 'udp' => (init_arg   => undef,
                   is         => 'ro',
                   isa        => 'Net::BitTorrent::Network::UDP',
@@ -21,9 +22,5 @@ package Net::BitTorrent::DHT::Standalone;
                                   on_data_in => sub { $self->_on_data_in(@_) }
         );
     }
-    has 'port' => (is      => 'ro',
-                   default => '0',
-                   isa     => 'Int'
-    );
 }
 1;
