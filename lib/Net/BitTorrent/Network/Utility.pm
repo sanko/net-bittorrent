1f0970e78c9a3c0009fecda441ba945039a3dd7e
Sanko Robinson <sanko@cpan.org>
2010-07-03 12:59:18 +0800

    Adding ip2paddr($) network utility function

--- lib/Net/BitTorrent/Network/Utility.pm
+++ lib/Net/BitTorrent/Network/Utility.pm
@@ -13,7 +13,7 @@ package Net::BitTorrent::Network::Utility;
     require Exporter;
     our @ISA = qw[Exporter];
     our %EXPORT_TAGS = (socket   => [qw[client server]],
-                        paddr    => [qw[sockaddr paddr2ip]],
+                        paddr    => [qw[sockaddr paddr2ip ip2paddr]],
                         sockaddr => [qw[pack_sockaddr unpack_sockaddr]],
                         vars     => [qw[%cache]]
     );
@@ -44,12 +44,42 @@ package Net::BitTorrent::Network::Utility;
         $hex[8] = $hex[7] >> 8;
         $hex[7] = $hex[6] & 0xff;
         $hex[6] >>= 8;
-        my $return = sprintf("%X:%X:%X:%X:%X:%X:%D:%D:%D:%D", @hex);
-        $return =~ s/(0+:)+/:/;
-        $return =~ s/^0+//;
-        $return =~ s/^:+/::/;
-        $return =~ s/::0+/::/;
+        my $return = sprintf '%X:%X:%X:%X:%X:%X:%D:%D:%D:%D', @hex;
+        $return =~ s|(0+:)+ |:|x;
+        $return =~ s|^0+     ||x;
+        $return =~ s|^:+   |::|x;
+        $return =~ s|::0+  |::|x;
         return $return;
+    }
+
+    sub ip2paddr ($) {
+        my ($addr) = @_;
+        $addr = '::' . $addr unless $addr =~ /:/;
+        if ($addr =~ /^(.*:)(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/)
+        {    # mixed hex, dot-quad
+            return undef if $2 > 255 || $3 > 255 || $4 > 255 || $5 > 255;
+            $addr = sprintf('%s%X%02X:%X%02X', $1, $2, $3, $4, $5)
+                ;    # convert to pure hex
+        }
+        my $c;
+        return undef
+            if $addr =~ /[^:\da-f]/i ||    # non-hex character
+                (($c = $addr) =~ s/::/x/ && $c =~ /(?:x|:):/)
+                ||                         # double :: ::?
+                $addr =~ /[0-9a-fA-F]{5,}/;    # more than 4 digits
+        $c = $addr =~ tr[:][:];                # count the colons
+        return undef if $c < 7 && $addr !~ /::/;
+        if ($c > 7) {                          # strip leading or trailing ::
+            return undef
+                unless $addr =~ s|^::|:|
+                    || $addr =~ s|::$|:|;
+            return undef if --$c > 7;
+        }
+        $addr =~ s|::|:::| while $c++ < 7;     # expand compressed fields
+        $addr .= 0 if $addr =~ m[:$];
+        my @hex = split ':', $addr;
+        $hex[$_] = hex $hex[$_] || 0 for 0 .. $#hex;
+        return pack 'n8', @hex;
     }
 
     sub pack_sockaddr {
