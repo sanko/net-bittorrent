e17897c3464348ef984707f29ca604103182ae78
Sanko Robinson <sanko@cpan.org>
2010-07-22 11:21:01 +0800

    Adding ::Torrent::Piece::Block class to MANIFEST and repo

---
+++ lib/Net/BitTorrent/Torrent/Piece/Block.pm
@@ -0,0 +1,29 @@
+{
+
+    package Net::BitTorrent::Torrent::Piece::Block;
+    use Moose;
+    our $MAJOR = 0.074; our $MINOR = 0; our $DEV = 1; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
+    has 'offset' => (isa      => 'Int',
+                     is       => 'ro',
+                     required => 1
+    );
+    has 'piece' => (isa      => 'Net::BitTorrent::Torrent::Piece',
+                    is       => 'ro',
+                    required => 1,
+                    weak_ref => 1,
+                    handles => [qw[index]]
+    );
+    has 'length' => (isa      => 'Int',
+                     is       => 'ro',
+                     required => 1
+    );
+    has 'peer' => (isa      => 'Net::BitTorrent::Peer',
+                   is       => 'ro',
+                   #required => 1,
+                   writer => '_assign_peer',
+                   predicate => '_has_peer',
+                   weak_ref => 1
+
+    );
+}
+1;
