459691a57ff00c6cb2ca171edddbca1aa79cbd07
Sanko Robinson <sanko@cpan.org>
2010-07-22 11:30:41 +0800

    Adding ::Torrent::Piece to MANIFEST and repo

---
+++ lib/Net/BitTorrent/Torrent/Piece.pm
@@ -0,0 +1,90 @@
+{
+
+    package Net::BitTorrent::Torrent::Piece;
+    use Moose;
+    our $MAJOR = 0.074; our $MINOR = 0; our $DEV = 1; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
+    has 'index' => (isa      => 'Int',
+                    is       => 'ro',
+                    required => 1
+    );
+    has 'selector' => (
+                    isa      => 'Net::BitTorrent::Torrent::PieceSelector',
+                    is       => 'ro',
+                    required => 1,
+                    handles => [qw[end_game strategy torrent working_pieces]],
+                    weak_ref => 1
+    );
+    has 'length' => (isa        => 'Int',
+                     is         => 'ro',
+                     lazy_build => 1
+    );
+
+    sub _build_length {
+        my $s = shift;
+        return $s->torrent->length % $s->torrent->piece_length
+            if $s->index == $s->torrent->piece_count;
+        return $s->torrent->piece_length;
+    }
+    has 'priority' => (
+            isa     => 'Int',
+            is      => 'ro',
+            writer  => '_set_priority',
+            default => 2                  # 1:low/slow, 2:normal, 3: high/fast
+    );
+    has 'block_length' => (isa        => 'Int',
+                           is         => 'ro',
+                           lazy_build => 1
+    );
+
+    sub _build_block_length {
+        my $s   = shift;
+        my $max = 2**14;
+        return $max > $s->length ? $s->length : $max;
+    }
+    has 'blocks' => (
+            isa        => 'ArrayRef[Net::BitTorrent::Torrent::Piece::Block]',
+            is         => 'ro',
+            lazy_build => 1,
+            traits     => ['Array'],
+            handles =>
+                {_first_unassigned_block => ['first', sub { !$_->_has_peer }]}
+    );
+
+    sub _build_blocks {
+        my $s = shift;
+        warn int($s->length / $s->block_length);
+        warn $s->length % $s->block_length;
+        require Net::BitTorrent::Torrent::Piece::Block;
+        my $offset = 0;
+        my @blocks = map {
+            my $b =
+                Net::BitTorrent::Torrent::Piece::Block->new(
+                                                    piece  => $s,
+                                                    offset => $offset,
+                                                    length => $s->block_length
+                );
+            $offset += $s->block_length;
+            $b
+        } 1 .. int($s->length / $s->block_length);
+        push @blocks,
+            Net::BitTorrent::Torrent::Piece::Block->new(
+                                  piece  => $s,
+                                  offset => $offset,
+                                  length => int($s->length % $s->block_length)
+            ) if $s->length % $s->block_length;
+        use Data::Dump;
+        ddx \@blocks;
+        return \@blocks;
+    }
+
+    sub _assign_block {
+        my ($s, $p) = @_;
+        my ($o, $l) = (0, 0);
+        require Net::BitTorrent::Torrent::Piece::Block;
+        my $b = $s->_first_unassigned_block();
+        return if !$b;
+        $b->_assign_peer($p);
+        $b;
+    }
+}
+1;
