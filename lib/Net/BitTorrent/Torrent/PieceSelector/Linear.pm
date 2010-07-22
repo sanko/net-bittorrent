b11b400148bbb089d842df1b3376c92773cb0616
Sanko Robinson <sanko@cpan.org>
2010-07-22 11:36:23 +0800

    Adding simple random and sequential piece selectors

--- lib/Net/BitTorrent/Torrent/PieceSelector/Linear.pm
+++ lib/Net/BitTorrent/Torrent/PieceSelector/Linear.pm
@@ -1,5 +1,14 @@
 {
+
     package Net::BitTorrent::Torrent::PieceSelector::Linear;
     use Moose::Role;
+    our $MAJOR = 0.074; our $MINOR = 0; our $DEV = 1; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
+
+    sub _select_piece {
+        my ($s, $peer) = @_;
+
+  # XXX - Make sure this isn't a piece we've requested all blocks from already
+        index $peer->_pieces_intersection->to_Bin, '1';
+    }
 }
 1;
--- lib/Net/BitTorrent/Torrent/PieceSelector/Random.pm
+++ lib/Net/BitTorrent/Torrent/PieceSelector/Random.pm
@@ -1,5 +1,25 @@
 {
+
     package Net::BitTorrent::Torrent::PieceSelector::Random;
     use Moose::Role;
+    our $MAJOR = 0.074; our $MINOR = 0; our $DEV = 1; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
+
+    sub _select_piece {
+        my ($s, $peer) = @_;
+        my $bitfield = $peer->_pieces_intersection->to_Bin;
+        warn $bitfield;
+        my @index;
+        {
+            my $i = index $bitfield, '1', 0;
+            while ($i != -1) {
+                push @index, $i;
+                $i = index $bitfield, '1', $i + 1;
+            }
+        }
+
+  # XXX - Make sure this isn't a piece we've requested all blocks from already
+        return if !scalar @index;
+        return $index[rand @index];
+    }
 }
 1;
